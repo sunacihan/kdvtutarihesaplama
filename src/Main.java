417056606c8b170f27b991b46f897f1dcf5aa94f
furkanyaylacesme <furkanyaylacesme@gmail.com>
2023-02-20 21:28:44 +0300

    kdv tutarı hesaplandı

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 ,
+        // tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
+
+        Scanner input= new Scanner(System.in);
+        double g_sayi=input.nextDouble();
+        double kdvli_fiyat;
+        if (0<g_sayi && g_sayi<=1000){
+            kdvli_fiyat=g_sayi*0.18;
+            System.out.println(kdvli_fiyat);
+        } else if (g_sayi>1000) {
+            kdvli_fiyat=g_sayi*0.08;
+            System.out.println(kdvli_fiyat);
+        }
+        else {
+            System.out.println("Hata! Sıfırdan küçük sayı girdiniz.");
+        }
     }
 }
